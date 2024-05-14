740659adda0fb2433a6c51c75354ff2143a55924
PedroBarbosa <psbpedrobarbosa@gmail.com>
2024-05-14 18:07:08 +0100

    Update latest results

--- notebooks/3_MOTIF_BASED_STANDARD_ML/concat_into_1_df.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-import glob
-
-files = glob.glob('*.tsv.gz')
-
-df = pd.concat(map(lambda file: pd.read_csv(file, sep="\t"), files))
-df.to_csv('ALL_CONCAT.tsv.gz', sep='\t', compression='gzip', index=False)
